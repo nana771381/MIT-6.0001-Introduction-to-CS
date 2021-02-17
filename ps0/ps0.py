85a41f42679b029ce837fe1a48e2c82f58cf8ad0
Emily <76611914+nana771381@users.noreply.github.com>
2021-02-17 13:28:55 +0800

    Add files via upload

---
+++ ps0/ps0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 16 12:40:43 2021
+
+@author: Windows10
+"""
+
+import math
+
+x = input("Enter a number:")
+y = input("Enter a number:")
+
+print(int(x)**int(y))
+print(math.log(int(x),2))
+
+
