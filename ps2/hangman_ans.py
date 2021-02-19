bc6ad090a4d1a120a001eba87c1854e567e4c3d6
Emily <76611914+nana771381@users.noreply.github.com>
2021-02-19 18:36:40 +0800

    Update hangman_ans.py

--- ps2/hangman_ans.py
+++ ps2/hangman_ans.py
@@ -468,4 +468,4 @@ if __name__ == "__main__":
     # uncomment the following two lines. 
     
     secret_word = choose_word(wordlist)
-    hangman_with_hints("apple")
+    hangman_with_hints(secret_word)
